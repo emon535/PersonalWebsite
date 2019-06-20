98522dc27bd91ef2560a96897ddee86cb10378ff
Foysal Ahmed Emon <emon535@gmail.com>
2019-06-20 17:22:25 +0600

    added photos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './images/phoyo_circle.png';
 import './App.css';
 
 function App() {
@@ -8,18 +8,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome <code>to</code> my Site.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
-    </div>
+    </div >
   );
 }
 
