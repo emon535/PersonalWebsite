e7a5e5a40dacce414674a4a72b89bcf4832db015
Foysal Ahmed Emon <emon535@gmail.com>
2019-06-23 12:27:49 +0600

    updated with CV

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import logo from './images/phoyo_circle.png';
 import './App.css';
-
+import cv from './resources/foysal_ahmed_emon_cv_23june.pdf'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome <code>to</code> my Site.
-        </p>
       </header>
+      <div className="App-body">
+        <br></br>
+        Welcome <code>to</code> my Site.
+          <br></br>
+        <small>Download the <a href={cv}>CV </a></small>
+      </div>
     </div >
   );
 }
