1b1016be9e3ec6fdd61a2fd9985adcc4435d95c5
Foysal Ahmed Emon <emon535@gmail.com>
2019-07-22 03:04:30 +0600

    hover active color

--- src/components/Experience/Experiences.js
+++ src/components/Experience/Experiences.js
@@ -5,7 +5,6 @@ export default class Experiences extends Component {
   render() {
     return (
       <div className="experience">
-        {" "}
         <h1>Experiences</h1>
       </div>
     );
