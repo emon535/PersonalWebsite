b1d5207ed5499df61c723a8072c804506bfaddda
Foysal Ahmed Emon <emon535@gmail.com>
2019-07-22 03:13:23 +0600

    moved to col, about icon

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -45,15 +45,17 @@ export default class About extends Component {
         <div className="about">
           <Col className="about-description">
             <Row className="about-body">
-              <h1 className="disp">
-                FOYSAL AHMED <span>EMON</span>
-              </h1>
-              <h3>
-                190 EAST KAZIPARA, KAFRUL, DHAKA <br /> (+880) 1520100178
-                <br /> <span>EMON535@GMAIL.COM</span>
-              </h3>
-              <h5>{ABOUT_DATA.coverLetter}</h5>
-              <hr />
+              <Col>
+                <h1 className="disp">
+                  FOYSAL AHMED <span>EMON</span>
+                </h1>
+                <h3>
+                  190 EAST KAZIPARA, KAFRUL, DHAKA <br /> (+880) 1520100178
+                  <br /> <span>EMON535@GMAIL.COM</span>
+                </h3>
+                <h5>{ABOUT_DATA.coverLetter}</h5>
+                <hr />
+              </Col>
             </Row>
 
             {this._getSocial()}
