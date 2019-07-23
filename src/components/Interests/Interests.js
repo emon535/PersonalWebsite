ae2f4d9df2dc792990523bae8db72e23253233a2
Foysal Ahmed Emon <emon535@gmail.com>
2019-07-23 23:33:01 +0600

    Added Interest Pages

---
+++ src/components/Interests/Interests.js
@@ -0,0 +1,11 @@
+import react, { Component } from "react";
+
+export default class extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Interest</h1>
+      </React.Fragment>
+    );
+  }
+}
