63d5c575682ad4381aee52303244a83e211149a0
Foysal Ahmed Emon <emon535@gmail.com>
2019-08-14 12:42:58 +0600

    Experience updated

--- src/components/Experience/Experiences.js
+++ src/components/Experience/Experiences.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./Experience.css";
-
+import infolytx from "./../../images/infolytx.png";
+import bm from "./../../images/bm.png";
 import { Row, Col } from "reactstrap";
 
 export default class Experiences extends Component {
@@ -12,14 +13,41 @@ export default class Experiences extends Component {
             <Col>
               <h1 className="disp">EXPERIENCES</h1>
               <hr />
-              <h4>INFOLYTX</h4>
-              Software Engineer <br /> Feb, 2019 - Present
+              <Row>
+                <Col sm={2}>
+                  <div className="work-logo">
+                    <img src={infolytx} />
+                  </div>
+                </Col>
+                <Col sm={10}>
+                  <h4>INFOLYTX</h4>
+                  Software Engineer <br /> Feb, 2019 - Present
+                </Col>
+              </Row>
               <hr />
-              <h4>BM IT Solutions</h4>
-              Software Engineer <br /> Feb, 2018 - Jan, 2019
+              <Row>
+                <Col sm={2}>
+                  <div className="work-logo">
+                    <img src={bm} />
+                  </div>
+                </Col>
+                <Col sm={10}>
+                  <h4>BM IT Solutions</h4>
+                  Software Engineer <br /> Feb, 2018 - Jan, 2019
+                </Col>
+              </Row>
               <hr />
-              <h4>INFOLYTX</h4>
-              Junior Software Engineer <br /> Dec, 2015 - Jan 2018
+              <Row>
+                <Col sm={2}>
+                  <div className="work-logo">
+                    <img src={infolytx} />
+                  </div>
+                </Col>
+                <Col sm={10}>
+                  <h4>INFOLYTX</h4>
+                  Junior Software Engineer <br /> Dec, 2015 - Jan 2018
+                </Col>
+              </Row>
             </Col>
           </Row>
         </Col>
