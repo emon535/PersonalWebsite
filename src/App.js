a8037251e71cd35769a978b6dea29d44bf029b7f
emon535 <emon535@gmail.com>
2023-07-16 22:28:41 +0100

    Restructure code

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
-import React from "react";
-import "./App.css";
-import cv from "./resources/foysal_ahmed_emon_cv_23june.pdf";
-import Protfollio from "./components/Protfollio/Protfollio";
-import About from "./components/About/About";
-import Menu from "./components/SideMenu/Menu";
-import { Row, Col } from "reactstrap";
-import Education from "./components/Education/Education";
-import Experiences from "./components/Experience/Experiences";
-import Skills from "./components/Skills/Skills";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div className="App-body">
-        <Row>
-          <Col sm={3}>
-            <Menu />
-          </Col>
-          <Col sm={9}>
-            <About />
-            <Experiences />
-            <Education />
-            <Skills />
-            <Protfollio />
-          </Col>
-        </Row>
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
